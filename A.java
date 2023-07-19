6f22a8e8ece76561dee8b583f0801b23575da39d
Abhilasha <pabhilashapatel09@gmail.com>
2023-07-19 13:59:26 +0530

    first commit

---
+++ A.java
@@ -0,0 +1,26 @@
+class A{
+
+public static void main(String[] args){
+int i=10;
+double d=123;
+byte b=125;
+ char c='A';
+boolean p=true;
+float f=12f;
+long l=123;
+System.out.println(i);
+System.out.println(d);
+System.out.println(b);
+System.out.println(p);
+System.out.println(p);
+System.out.println(c);
+System.out.println(i);
+
+
+
+
+}
+
+
+
+}
